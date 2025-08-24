ebd8b48d149b49541d7d4d4cd56c15fd01b079e6
constyejin <lightup.jin@gmail.com>
2025-08-24 21:31:34 +0800

    ✨ ddg_culture slide 기능 구현중

--- js/partnersRolling.js
+++ js/partnersRolling.js
@@ -19,3 +19,49 @@ gsap.to(list, {
     })
   }
 });
+
+
+const inner = document.querySelector(".culture_inner");
+const items = gsap.utils.toArray(".culture_item");
+
+inner.innerHTML += inner.innerHTML; 
+
+let totalHeight = inner.scrollHeight / 2; 
+
+gsap.to(inner, {
+  y: -totalHeight, 
+  duration: 40,     
+  ease: "linear",
+  repeat: -1
+});
+
+gsap.to(items, {
+  yPercent: -200, 
+  duration: 40,
+  ease: "linear",
+  repeat: -1,
+  modifiers: {
+    yPercent: gsap.utils.unitize(y => {
+      const wrapped = y % 200; 
+      return wrapped;
+    })
+  },
+  onUpdate: function() {
+    document.querySelectorAll(".culture_item").forEach(item => {
+      let rect = item.getBoundingClientRect();
+      let parentRect = document.querySelector(".culture_list").getBoundingClientRect();
+
+      let fadeZone = parentRect.height * 0.2;
+      let distTop = rect.top - parentRect.top;
+      let distBottom = parentRect.bottom - rect.bottom;
+
+      if (distTop < fadeZone) {
+        item.style.opacity = distTop / fadeZone; 
+      } else if (distBottom < fadeZone) {
+        item.style.opacity = distBottom / fadeZone; 
+      } else {
+        item.style.opacity = 1;
+      }
+    });
+  }
+});
